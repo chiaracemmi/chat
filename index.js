9d254a508ed602ad5dbe6d878afca28826eb91a2
Chiara Cemmi <cemmichiara@gmail.com>
2014-08-11 16:16:48 +0200

    v2
    v2

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var broad = "nuovo utente connesso";
+
+
+
+
+
+
+
 
 
 app.get('/', function(req, res){
@@ -12,10 +20,17 @@ http.listen(3000, function(){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  	io.emit('user connection', broad);
+  	socket.on("typing", function(data) {  
+    	io.sockets.emit("isTyping", {isTyping: data});
+	});
+
+  	socket.on('chat message', function(msg){
+    	io.emit('chat message', msg);
+
   });
 });
+
 
 
 /*per disattivare il server node su terminale schiccio ctrl * c. per riattivarlo node index*/
